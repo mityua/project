89b4769f17d53ee94262edd399611d185c9065df
Your Name <you@example.com>
2020-10-17 22:06:31 +0300

    make revertLetters() not static, replace space by underscore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        revertLetters(scanner);
-
+        String input = scanner.nextLine();
+        main.revertLetters(input);
         String condition;
         while (true) {
             System.out.println("Do you want to continue [y/n]?");
             condition = scanner.nextLine();
             if (condition.startsWith("y")) {
-                revertLetters(scanner);
+                main.revertLetters(input);
             } else {
                 break;
             }
         }
         System.out.println("Finish");
     }
-
-    private static void revertLetters(Scanner scanner) {
-        String input1 = scanner.nextLine();
+    private void revertLetters(String input1) {
         char[] inputLetters1 = input1.toCharArray();
         char[] outputLetters1 = new char[inputLetters1.length];
         for (int j = 0, i = inputLetters1.length - 1; i >= 0; j++, i--) {
-            outputLetters1[j] = inputLetters1[i];
+            if (inputLetters1[i] == ' ') {
+                outputLetters1[j] = '_';
+            } else {
+                outputLetters1[j] = inputLetters1[i];
+            }
         }
         System.out.println(new String(outputLetters1));
     }
